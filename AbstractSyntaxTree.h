aaf47ec6495f6eb0eb3f1489f2be8a3887fc5a01
alexge50 <alexge50.x@gmail.com>
2017-08-05 09:19:01 +0300

    synced

--- AbstractSyntaxTree.h
+++ AbstractSyntaxTree.h
@@ -1,5 +1,5 @@
 /*
- * Abstract Syntax Tree.h
+ * AbstractSyntaxTree.h
  *
  *  Created on: Aug 5, 2017
  *      Author: alex
@@ -8,27 +8,27 @@
 #ifndef SRC_ABSTRACT_SYNTAX_TREE_H_
 #define SRC_ABSTRACT_SYNTAX_TREE_H_
 
-#include "Abstract Syntax Tree/Comparation.h"
-#include "Abstract Syntax Tree/ConstNumber.h"
-#include "Abstract Syntax Tree/Expression.h"
-#include "Abstract Syntax Tree/If.h"
-#include "Abstract Syntax Tree/InstructionBlock.h"
-#include "Abstract Syntax Tree/Instruction.h"
-#include "Abstract Syntax Tree/NewStack.h"
-#include "Abstract Syntax Tree/NextElement.h"
-#include "Abstract Syntax Tree/PopStack.h"
-#include "Abstract Syntax Tree/Program.h"
-#include "Abstract Syntax Tree/PushStack.h"
-#include "Abstract Syntax Tree/ResetStack.h"
-#include "Abstract Syntax Tree/SignalInstruction.h"
-#include "Abstract Syntax Tree/While.h"
-#include "Abstract Syntax Tree/Comparation/Equal.h"
-#include "Abstract Syntax Tree/Comparation/GreaterThan.h"
-#include "Abstract Syntax Tree/Comparation/LessThan.h"
-#include "Abstract Syntax Tree/Expression/Addition.h"
-#include "Abstract Syntax Tree/Expression/Division.h"
-#include "Abstract Syntax Tree/Expression/Mod.h"
-#include "Abstract Syntax Tree/Expression/Multiplication.h"
-#include "Abstract Syntax Tree/Expression/Subtraction.h"
+#include "AbstractSyntaxTree/Comparation.h"
+#include "AbstractSyntaxTree/ConstNumber.h"
+#include "AbstractSyntaxTree/Expression.h"
+#include "AbstractSyntaxTree/If.h"
+#include "AbstractSyntaxTree/InstructionBlock.h"
+#include "AbstractSyntaxTree/Instruction.h"
+#include "AbstractSyntaxTree/NewStack.h"
+#include "AbstractSyntaxTree/NextElement.h"
+#include "AbstractSyntaxTree/PopStack.h"
+#include "AbstractSyntaxTree/Program.h"
+#include "AbstractSyntaxTree/PushStack.h"
+#include "AbstractSyntaxTree/ResetStack.h"
+#include "AbstractSyntaxTree/SignalInstruction.h"
+#include "AbstractSyntaxTree/While.h"
+#include "AbstractSyntaxTree/Comparation/Equal.h"
+#include "AbstractSyntaxTree/Comparation/GreaterThan.h"
+#include "AbstractSyntaxTree/Comparation/LessThan.h"
+#include "AbstractSyntaxTree/Expression/Addition.h"
+#include "AbstractSyntaxTree/Expression/Division.h"
+#include "AbstractSyntaxTree/Expression/Mod.h"
+#include "AbstractSyntaxTree/Expression/Multiplication.h"
+#include "AbstractSyntaxTree/Expression/Subtraction.h"
 
 #endif /* SRC_ABSTRACT_SYNTAX_TREE_H_ */
